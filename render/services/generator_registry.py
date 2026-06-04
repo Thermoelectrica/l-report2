c543950adb46deb4aae5ddef286907b1b7221e5d
vova <vova@lenovo.redos>
2026-06-04 13:55:53 +0300

    Удаление Pandoc

--- render/services/generator_registry.py
+++ render/services/generator_registry.py
@@ -63,7 +63,5 @@ generator_registry = GeneratorRegistry()
 
 # Register default generators
 from .weasyprint_generator import weasyprint_generator
-from .pandoc_docx_generator import pandoc_docx_generator
 
 generator_registry.register(weasyprint_generator)
-generator_registry.register(pandoc_docx_generator)
